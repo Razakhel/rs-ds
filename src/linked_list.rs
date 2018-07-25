9ef934fec20e7e0842e66c9c66d3583df4f53d6f
Razakhel <romain.milbert@gmail.com>
2018-07-25 12:52:11 +0200

    [Update] Added List::push() function to add elements

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 #[derive(Debug)]
 pub struct Node<T> {
   value: T,
@@ -15,4 +17,13 @@ pub struct List<T> {
 
 impl<T> List<T> {
   pub fn new(root: Option<Box<Node<T>>>) -> Self { List { root } }
+
+  pub fn push(&mut self, value: T) {
+    let new_node = Node {
+      value,
+      next: mem::replace(&mut self.root, None)
+    };
+
+    self.root = Some(Box::new(new_node))
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,17 @@ use linked_list::List;
 use linked_list::Node;
 
 fn main() {
-  let list = List::new(Some(Box::new(Node::new(3.25f32, None))));
+  let mut list = List::new(Some(Box::new(Node::new(3.25f32, None))));
+
+  list.push(34.0f32);
+  list.push(2.134f32);
+  list.push(12.04f32);
+  list.push(85.01f32);
+  list.push(1.91f32);
+  list.push(43723.0f32);
+  list.push(321.2f32);
+  list.push(853.654f32);
+  list.push(59271.4f32);
 
   println!("{:?}", list);
 }
